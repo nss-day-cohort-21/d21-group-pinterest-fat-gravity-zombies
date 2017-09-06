6f9f70ea8a4febb7609731dc85398b4f9402db33
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-06 05:44:18 -0500

    merging to master

--- app/app.js
+++ app/app.js
@@ -1,3 +1,22 @@
 "use strict";
 
 const app = angular.module('Pinterest', ['ngRoute']);
+
+app.config(($routeProvider) => {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'partials/register-login.html',
+		controller: 'userCtrl'
+	})
+	.otherwise('/');
+});
+
+//starts app with firebase credentials from ./app/values/fb-creds.js
+app.run(($location, FBCreds) => {
+	firebase.initializeApp(FBCreds);
+});
+
+//create userCtrl
+//create dummy text on register-login.html
+//create isAuthenticated
+//
--- app/factories/factory-user.js
+++ app/factories/factory-user.js
@@ -1,1 +1,39 @@
 "use strict";
+
+app.factory('userFactory', function($q, $http) {
+
+	let currentUser = null;
+
+	let googleProvider = new firebase.auth.GoogleAuthProvider();
+
+	let getCurrentUser = function () {
+		return currentUser;
+	};
+
+	let isAuthenticated = function () {
+		return $q((resolve, reject) => {
+			firebase.auth().onAuthStateChanged((user) => {
+				if (user) {
+					currentUser = user.uid;
+					resolve(true);
+				} else {
+					reject(false);
+				}
+			});
+		});
+	};
+
+	let logIn = function(user) {
+		return firebase.auth().signInWithEmailAndPassword(user.email, user.password);
+	};
+
+	let logOut = function() {
+		return firebase.auth().signOut();
+	};
+
+	return {getCurrentUser, isAuthenticated, logIn, logOut};
+
+});
+
+
+//connect logIn / Logout functions to the DOM
