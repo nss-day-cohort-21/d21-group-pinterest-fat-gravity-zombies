ad47c7eae3c4b72d95a1b4d3c41dde47af8c115d
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-07 12:54:47 -0500

    setup boards controller

--- app/controllers/control-boards-list.js
+++ app/controllers/control-boards-list.js
@@ -1,5 +1,16 @@
 "use strict";
 
-app.controller('BoardsCrtl', function($scope, boardFactory) {
+app.controller('BoardsCrtl', function($scope, boardFactory, userFactory) {
+
+	$scope.myBoards = [];
+
+	let showMyBoards = () => {
+		boardFactory.getUserBoards(userFactory.getCurrentUser())
+		.then((data) => {
+			$scope.myBoards = data;
+			console.log("$scope.myBoards", $scope.myBoards);
+		});
+	};
+	showMyBoards();
 
 });
