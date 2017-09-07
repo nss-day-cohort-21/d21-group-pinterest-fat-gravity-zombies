fce87371fdd1c2f2ac41247ce02a5dc9982c3c98
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-07 09:35:34 -0500

    function to get all pins in db

--- app/controllers/control-pins-list.js
+++ app/controllers/control-pins-list.js
@@ -1,1 +1,16 @@
 "use strict";
+
+app.controller('pinsListCtrl', function ($scope, pinFactory) {
+
+	let pinsData = [];
+
+	const showAllPins = () => {
+		pinFactory.getAllPins()
+			.then((data) => {
+				console.log("data from showAllPins", data);
+			});
+	};
+
+	showAllPins();
+
+});
