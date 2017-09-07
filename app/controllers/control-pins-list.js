cd602a5ef57dc27c00dd82105c463c23a82d8d26
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-07 10:55:28 -0500

    prints associated board with each pin

--- app/controllers/control-pins-list.js
+++ app/controllers/control-pins-list.js
@@ -1,13 +1,19 @@
 "use strict";
-app.controller('pinsListCtrl', function ($scope, pinFactory) {
+app.controller('pinsListCtrl', function ($scope, pinFactory, boardFactory) {
 
 	$scope.pinsData = [];
+	$scope.boardsData = [];
 
 	const showAllPins = () => {
 		pinFactory.getAllPins()
 			.then((data) => {
 				$scope.pinsData = data;
 				console.log("data from pinsData", $scope.pinsData);
+			});
+		boardFactory.getAllBoards()
+			.then((data) => {
+				$scope.boardsData = data;
+				console.log("boards data from showAllPins", data);
 			});
 	};
 
