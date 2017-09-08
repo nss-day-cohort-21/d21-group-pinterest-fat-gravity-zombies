907ab1b9c303a7ffc3cffd01230c31b4c03ed1a8
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-07 20:34:39 -0500

    adding new pins to db and refreshing my pins page

--- app/app.js
+++ app/app.js
@@ -31,6 +31,11 @@ app.config(($routeProvider) => {
 		resolve: {isAuth}
 
 	})
+	.when('/new-pin', {
+		templateUrl: 'partials/new-pin.html',
+		controller: 'myPinsListCtrl',
+		resolve: {isAuth}
+	})
 	.when('/MyBoards', {
 		templateUrl: 'partials/boards-view.html',
 		controller: 'BoardsCtrl',
